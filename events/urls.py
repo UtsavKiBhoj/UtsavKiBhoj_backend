a1ea9e095ae150b07f0e4f955fd3cdc4803c666a
Vivek <vivek.bhadouria@kylient.com>
2024-11-08 11:45:24 +0530

    update ulrs

--- events/urls.py
+++ events/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import CreateEventView, CreateEventLocationView, EventListView, EventDetailView
 
 urlpatterns = [
-    path('Create-form/', CreateEventView.as_view(), name='event_create'),
-    path('event-locations/', CreateEventLocationView.as_view(), name='event_location_create'),
+    path('Create-form/', CreateEventView.as_view(), name='event_details'),
+    path('location-details/', CreateEventLocationView.as_view(), name='event_location_details'),
     path('list/', EventListView.as_view(), name='events_list'),
-    path('details/<int:event_id>', EventDetailView.as_view(), name='event_details'),
+    path('detail/<int:event_id>', EventDetailView.as_view(), name='event_details'),
 ]
