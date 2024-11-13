531206f617eefe0c7b630a0314beafbb06fd0f63
Vivek <vivek.bhadouria@kylient.com>
2024-11-13 17:54:14 +0530

    Solve the errors of location API and merge the event location data in the eventList api response

--- events/serializers.py
+++ events/serializers.py
@@ -3,13 +3,26 @@ from .models import Event, EventLocation
 from django.utils import timezone
 from user.serializers import User_Serializer
 
+# EventLocationSerializer 
+class EventLocationSerializer(serializers.ModelSerializer):
+    event = serializers.PrimaryKeyRelatedField(queryset=Event.objects.all())
+
+    class Meta:
+        model = EventLocation
+        fields = ['location_id', 'location_name', 'address', 'pin_code', 'landmark', 'event']
+
+    def create(self, validated_data):
+        # No need to manually fetch `event` since `PrimaryKeyRelatedField` does it
+        return EventLocation.objects.create(**validated_data)
 
 
+# Event serializer
 class EventSerializer(serializers.ModelSerializer):
     organizer = User_Serializer(read_only=True)
+    location_name = EventLocationSerializer(many=True, read_only=True)
     class Meta:
         model = Event
-        fields = ['event_id', 'event_name', 'description', 'date', 'organizer']
+        fields = ['event_id', 'event_name', 'description', 'date', 'organizer', 'location_name']
 
     def validate_event_name(self, value):
         if not value:
@@ -20,16 +33,3 @@ class EventSerializer(serializers.ModelSerializer):
         if value < timezone.now().date():
             raise serializers.ValidationError("Event date cannot be in the past.")
         return value
-
-# EventLocationSerializer 
-class EventLocationSerializer(serializers.ModelSerializer):
-    # Use `event` instead of `event_id` since it's a ForeignKey
-    event = serializers.PrimaryKeyRelatedField(queryset=Event.objects.all())
-
-    class Meta:
-        model = EventLocation
-        fields = ['location_name', 'address', 'pin_code', 'landmark', 'event']
-
-    def create(self, validated_data):
-        # No need to manually fetch `event` since `PrimaryKeyRelatedField` does it
-        return EventLocation.objects.create(**validated_data)
